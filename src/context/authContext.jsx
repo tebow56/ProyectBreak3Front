10a57004767132bb430eb658ff33364eca2426b0
tebow56 <158113814+tebow56@users.noreply.github.com>
2026-04-19 19:16:14 +0200

    Revert "cambio en context"
    This reverts commit 4345c05ac4640fe914bc4e26d96114caad71b26b.

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,11 +1,79 @@
-import { createContext, useContext } from "react";
 
-export const authContext = createContext();
+import { createContext, useContext, useState, useEffect } from "react";
+import { useBasic } from "./basicContext";
+import { useNavigate, useLocation } from "react-router-dom";
+
+
+
+const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:3003"
+
+const authContext = createContext();
+
 
 export const useAuth = () => {
-    const context = useContext(authContext);
-    if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider');
+    return useContext(authContext);
+}
+
+export const AuthProvider = ({ children }) => {
+        const { user, setUser, isLoading, setIsLoading, userData, setUserData } = useBasic();
+        const navigate = useNavigate();
+        const location = useLocation();
+        useEffect(() => {
+            
+            const fetchUser = async () => {
+                    if (location.pathname === '/login' || location.pathname === '/register') {
+                    return; 
+                }
+                setIsLoading(true)
+                    try {
+                    const response = await fetch(`${apiUrl}/API/auth/active-session`, {
+                        credentials: 'include'
+                    });
+                    if (response.ok) {
+                    const userInfo = await response.json();
+                    setUser(userInfo);
+                    if(!userData) {
+                        try {
+                            const findresponse= await fetch(`${apiUrl}/API/users/getbyemail/${userInfo.email}`, {
+                                credentials: 'include'
+                            });
+                            const newUserData= await findresponse.json()
+                            setUserData(newUserData)
+                        }catch (error){
+                            console.error('Error:', error)
+                        }}
+                    if (userInfo.admin === true && !location.pathname.startsWith('/admin')) {
+                            navigate('/admin');
+                    } else if (userInfo.admin === false && location.pathname.startsWith('/admin')) {
+                            navigate ('/user');
+                    }
+                    } else {
+                        setUser(null)    
+                        navigate('/login');
+                            
+                        }
+                    }
+                    catch (error) {
+                        console.error('Error:', error);
+                        setUser(null);
+                    } finally {
+                        setIsLoading(false);
+                    }
+
+            }
+            fetchUser();
+        }, [location.pathname, navigate, setUser, setIsLoading]);
+        if (isLoading) {
+        return (    
+            <div>
+                <p>Cargando...</p>
+            </div>
+        );
     }
-    return context;
-};
+
+    return (  
+        <authContext.Provider value={{ user, setUser, isLoading, setIsLoading }}>
+            {children}
+        </authContext.Provider>
+    )
+}
